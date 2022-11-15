91d76d88bbe0197f9b6b9c8c72adbd686b331497
SebastianJanczy <sebastian.janczy.collab@gmail.com>
2022-11-15 20:48:36 +0100

    Clear js

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,17 @@
 const navSlide = () => {
-
     const burger = document.querySelector('.burger-btn')
     const nav = document.querySelector('.nav-links')
     const navLink = document.querySelectorAll('.nav-links li')
 
-
-
-    // BURGER-BUTTON
     burger.addEventListener('click', () => {
         nav.classList.add('nav-active')
     })
-
     nav.addEventListener('mouseleave', () => {
         nav.classList.remove('nav-active')
     });
-
-
-
 }
-
-
-
 navSlide()
-
-
-
 const navBar = document.querySelector('.top-nav')
-
 window.onscroll = function() { scrollFunction() };
 
 let sticky = navBar.offsetTop;
