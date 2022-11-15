f34dab3c9eba77ed70fea36d84739df57aa63e07
SebastianJanczy <sebastian.janczy.collab@gmail.com>
2022-11-15 20:15:24 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const navSlide = () => {
+
+    const burger = document.querySelector('.burger-btn')
+    const nav = document.querySelector('.nav-links')
+    const navLink = document.querySelectorAll('.nav-links li')
+
+
+
+    // BURGER-BUTTON
+    burger.addEventListener('click', () => {
+        nav.classList.add('nav-active')
+    })
+
+    nav.addEventListener('mouseleave', () => {
+        nav.classList.remove('nav-active')
+    });
+
+
+
+}
+
+
+
+navSlide()
+
+
+
+const navBar = document.querySelector('.top-nav')
+
+window.onscroll = function() { scrollFunction() };
+
+let sticky = navBar.offsetTop;
+
+function scrollFunction() {
+    if (window.pageYOffset >= sticky) {
+        navBar.classList.add('sticky')
+    } else {
+        navBar.classList.remove('sticky')
+    }
+}
